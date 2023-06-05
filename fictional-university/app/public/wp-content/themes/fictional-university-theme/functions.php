cd0f78262dc05816e268c76abea5288fe2b8c5b5
ipakey <bornagainyfke@gmail.com>
2023-06-05 17:29:49 +0100

    wordpress upto lesson 17

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/functions.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,6 +1,8 @@
 <?php 
 
     function university_files(){
+
+
         wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
 
         wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -10,11 +12,14 @@
         wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
     
         wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
-
-
-    
     }
 
-    add_action('wp_enqueue_scripts','university_files'); 
+    add_action('wp_enqueue_scripts','university_files');
+
+    function university_features(){
+        add_theme_support('title-tag');
+    }
+
+    add_action('after_setup_theme', 'university_features'); 
     
 ?>
